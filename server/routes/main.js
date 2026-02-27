84a441ea05eba53a989c49514a86802aabecd93f
egejpg <egeergul67@gmail.com>
2026-02-27 21:59:15 +0300

    feat: implement basic EJS views

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-// Routes
-router.get('', (req, res) => {
-    const locals = {
-        title : "egejpg's blog",
-        description : "Simple blog made with Node.js, Express and MongoDB."
+const locals = {
+        title: "egejpg's blog",
+        description: "Simple blog created with Node.js, Express and MongoDB."
     }
 
+// Routes
+router.get('', (req, res) => {
+    
     res.render('index', { locals });
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
 });
 
 module.exports = router;
